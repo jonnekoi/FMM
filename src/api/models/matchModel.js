fa07e1db85f1d36815d33a8700441b43be39b1a9
jonnek <jonnekoivisto00@gmail.com>
2024-11-15 12:24:13 +0200

    addMatch works

--- src/api/models/matchModel.js
+++ src/api/models/matchModel.js
@@ -2,7 +2,7 @@ import promisePool from '../../utils/database.js';
 
 const fetchMatches = async () => {
   try {
-    const [rows] = await promisePool.query(`SELECT id, match_id, matchday, home_team, away_team, home_score, away_score FROM matches`);
+    const [rows] = await promisePool.query(`SELECT id, matchday, home_team, away_team, home_score, away_score FROM matches`);
     return rows;
   } catch (error) {
     console.error('Error fetching matches:', error);
